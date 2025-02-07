6866117135a24eafea5da1d2e3e909ce13e745d9
Ashwin <theashwinkarthikeyan@gmail.com>
2025-02-07 00:01:58 -0500

    updated clause.h

--- clause.h
+++ clause.h
@@ -5,6 +5,7 @@
 #ifndef ZKUNSAT_NEW_CLAUSE_H
 #define ZKUNSAT_NEW_CLAUSE_H
 
+#define INDEX_SZ 20
 #include "polynomial.h"
 
 inline uint64_t  get_negate(uint64_t encode){
@@ -21,9 +22,9 @@ public:
     }
 
 
-    clause(vector<uint64_t> & ells){
-        assert(!(ells.size() > DEGREE));
-        this->poly = polynomial(ells);
+    clause(vector<uint64_t> & ells, int deg){
+        assert(!(ells.size() > deg));
+        this->poly = polynomial(ells, deg);
         this->literals = ells;
     }
 
@@ -43,7 +44,7 @@ public:
 };
 
 
-inline std::vector<polynomial> witness_generator(clause& a, clause& b, clause& res, uint64_t& pivot) {
+inline std::vector<polynomial> witness_generator(clause& a, clause& b, clause& res, uint64_t& pivot, int deg) {
     GF2EX ap, bp, resp;
     GF2EX w0, w1;
     vector<uint64_t> pv{pivot};
@@ -63,32 +64,13 @@ inline std::vector<polynomial> witness_generator(clause& a, clause& b, clause& r
         assert(divide(w1, resp*neg_pivot_p, bp));
     }
     polynomial res0, res1;
-    GF2EX2polynomial(w0, res0);
-    GF2EX2polynomial(w1, res1);
+    GF2EX2polynomial(w0, res0, deg);
+    GF2EX2polynomial(w1, res1, deg);
     std::vector<polynomial> r{res0, res1};
     return  r;
 }
 
-inline void check_xres(clause& c0, clause& c1, clause cres, uint64_t pivot){
-    vector<polynomial> witness = witness_generator(c0, c1, cres, pivot);
-    vector<block> zero_coeff{zero_block, zero_block, zero_block};
-    vector<uint64_t> pivot_v{pivot};
-    vector<uint64_t> neg_pivot_v{get_negate(pivot)};
-
-    polynomial zero_p = polynomial(zero_coeff);
-    polynomial pivot_polynomial(pivot_v);
-    polynomial pivot_neg_polynomial(neg_pivot_v);
-    vector<polynomial> c0_res {c0.poly, cres.poly };
-    vector<polynomial> c1_res {c1.poly, cres.poly };
-    vector<polynomial> witness_pivot{witness[0], pivot_polynomial};
-    vector<polynomial> witness_neg_pivot{witness[1], pivot_neg_polynomial};
-    zero_p.InnerProductEqual(c0_res, witness_pivot);
-    zero_p.InnerProductEqual(c1_res, witness_neg_pivot);
-    pivot_neg_polynomial.ConverseCheck(pivot_polynomial);
-}
-
-
-inline clause get_res_f2k(const clause& a,  const clause& b, uint64_t pivot){
+inline clause get_res_f2k(const clause& a,  const clause& b, uint64_t pivot, int DEGREE){
     uint64_t  npivot  = get_negate(pivot);
 
     std::vector<uint64_t> altr = a.literals;
@@ -116,13 +98,11 @@ inline clause get_res_f2k(const clause& a,  const clause& b, uint64_t pivot){
         cout << endl;
         cout <<"overflow error!" << endl;
     }
-    padding(res_raw);
+    padding(res_raw, DEGREE);
     assert(res_raw.size() == DEGREE);
-    clause c(res_raw);
+    clause c(res_raw, DEGREE);
     return c;
 }
-
-
 
 
 
