a61e2b3839dca32afdce1cd925bab6a03500122c
Ashwin <theashwinkarthikeyan@gmail.com>
2025-02-25 22:06:34 -0500

    preprocess the skolem cert

---
+++ prover_backend/preprocess_skolem_cert.py
@@ -0,0 +1,22 @@
+import sys
+
+def format_certfile_and_print(certfile):
+    cert = open(certfile, 'r')
+    lines = cert.readlines
+
+    for line in lines:
+        words = line.split()
+        if len(words) == 3:
+            var = int(words[0])
+            inp1 = int(words[1])
+            inp2 = int(words[2])
+            if (inp1 == inp2 + 1) or (inp2 == inp1 + 1):
+                print(var, 0, 0)
+            else:
+                print(line)
+        else:
+            print(line)
+
+
+certfile = sys.argv[1]
+format_certfile_and_print(certfile)
