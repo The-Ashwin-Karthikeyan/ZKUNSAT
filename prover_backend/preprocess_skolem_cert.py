accee604ce3067b7adcea666aa2aeba746d65fd4
Ashwin <theashwinkarthikeyan@gmail.com>
2025-02-25 22:19:27 -0500

    fix preprocess bug

--- prover_backend/preprocess_skolem_cert.py
+++ prover_backend/preprocess_skolem_cert.py
@@ -2,7 +2,7 @@ import sys
 
 def format_certfile_and_print(certfile):
     cert = open(certfile, 'r')
-    lines = cert.readlines
+    lines = cert.readlines()
 
     for line in lines:
         words = line.split()
@@ -13,9 +13,9 @@ def format_certfile_and_print(certfile):
             if (inp1 == inp2 + 1) or (inp2 == inp1 + 1):
                 print(var, 0, 0)
             else:
-                print(line)
+                print(line[:-1])
         else:
-            print(line)
+            print(line[:-1])
 
 
 certfile = sys.argv[1]
