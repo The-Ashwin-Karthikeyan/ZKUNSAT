dd485d3b7a152611eb39ab109fcec192e4111532
Ashwin <theashwinkarthikeyan@gmail.com>
2025-03-29 16:31:22 -0400

    bug fix in script

--- prover_backend/get_inputs_for_zkqbf_caqe.sh
+++ prover_backend/get_inputs_for_zkqbf_caqe.sh
@@ -2,14 +2,16 @@
 # This script runs caqe on each instance in each subdirectory in <input_dir>.
 # In each subdirectory, it finds the one file (with a .qdimacs extension),
 # runs <caqe_dir>/./caqe on it
-# with a 300-second timeout. The output is saved to the corresponding .aag file.
+# with a 120-second timeout. The output is saved to the corresponding files.
 
 if [ "$#" -ne 5 ]; then
     echo "Usage: $0 <input_dir> <zkqbf_dir> <caqe_dir> <abc_dir> <aiger_dir>"
     echo " (1) <input_dir> should be the path to the directory containing the subdirectories."
     echo "     each subdirectory should contain exactly one file with a .qdimacs extension."
     echo " (2) <zkqbf_dir> should be the path to the zkqbf directory."
-    echo " (3) <caqe_dir> should be the path to the caqe directory."
+    echo " (3) <caqe_dir> should be the path to the cadet directory."
+    echo " (4) <abc_dir> should be the path to the abc directory."
+    echo " (5) <aiger_dir> should be the path to the aiger (toolset: https://github.com/arminbiere/aiger) directory."
     exit 1
 fi
 
@@ -30,28 +32,35 @@ for dir in $1/*/; do
   # Replace the .qdimacs extension with .prf
   renamedfile="${base%.qdimacs}_renamed.qdimacs"
   aagfile="${base%.qdimacs}_renamed.aag"
-  certfile="${base%.qdimacs}_renamed.cert"
+  aigfile="${base%.qdimacs}_renamed.aig"
+  minaigerfile="${base%.qdimacs}_renamed_min.aig"
+  minaagfile="${base%.qdimacs}_renamed_min.aag"
+  certfile="${base%.qdimacs}_renamed_min.cert"
   qmafile="${base%.qdimacs}_renamed.qma"
   cnffile="${base%.qdimacs}_renamed.cnf"
 
   # Full path for the proof file
   renamed_output="$dir$renamedfile"
   aag_output="$dir$aagfile"
+  aig_output="$dir$aigfile"
+  min_aiger_output="$dir$minaigerfile"
+  min_aag_output="$dir$minaagfile"
   cert_output="$dir$certfile"
   qma_output="$dir$qmafile"
   cnf_output="$dir$cnffile"
 
   # Run ../depqbf with a 30-second timeout and save the output to the proof file
-  timeout 120 python3 $2/prover_backend/rewrite_qdimacs.py "$file" > "$renamed_output"
-  timeout 300 $3/./caqe -c "$renamed_output" > "$aag_output"
+  timeout 30 python3 $2/prover_backend/rewrite_qdimacs.py "$file" > "$renamed_output"
+  timeout 120 $3/./caqe -c "$renamed_output" > "$aag_output"
   exit_status=$?
   # Check if the command timed out (exit status 124) or produced no output
   if [ $exit_status -eq 124 ] || [ ! -s "$aag_output" ]; then
     echo "caqe timed out or produced no output for $file; skipping to next input."
     continue  # Skips to the next iteration in the loop
   fi
-  timeout 120 python3 $2/prover_backend/cadet_preprocess.py "$aag_output" "$renamed_output" > "$cert_output"
-  max_var=$(python3 "$2/prover_backend/get_cert_maxvar.py" "$cert_output")
-  timeout 120 python3 $2/prover_backend/qdimacsmatrix_to_aig_andlines.py "$renamed_output" "$max_var" > "$qma_output"
-  timeout 120 python3 $2/prover_backend/combine_and_convert_aig_to_cnf.py "$cert_output" "$qma_output" > "$cnf_output"
+  timeout 30 $5/./aigtoaig "$aag_output" "$aig_output"
+  timeout 30 $4/abc -c "read $aig_output; strash; dc2; write $min_aiger_output;"
+  rm -f "$aig_output"
+  timeout 30 $5/./aigtoaig "$min_aiger_output" "$min_aag_output"
+  rm -f "$min_aiger_output"
 done
